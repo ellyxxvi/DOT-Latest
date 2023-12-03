7a45cdfbf2970cb49e727cf036afff8bb81d8d6a
Mikka Gabnete <mikkkayarana2@gmail.com>
2023-12-03 21:04:14 +0800

    see password admin side

--- admin/js/demo/login.js
+++ admin/js/demo/login.js
@@ -7,11 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeButton = staticBackdropModal._element.querySelector('.btn-close');
 
     if (loginButton) {
+       
+    
+        const passwordInput = document.getElementById('password');
+        const passwordToggle = document.getElementById('password-toggle');
+    
+        passwordToggle.addEventListener('click', function () {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                passwordToggle.classList.remove('fa-eye-slash');
+                passwordToggle.classList.add('fa-eye');
+            } else {
+                passwordInput.type = 'password';
+                passwordToggle.classList.remove('fa-eye');
+                passwordToggle.classList.add('fa-eye-slash');
+            }
+        });
+    
         loginButton.addEventListener('click', () => {
             const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
-
-            fetch(`${API_PROTOCOL}://${API_HOSTNAME}/auth/login`, { 
+            let password = document.getElementById('password').value; 
+            // Use the stored password variable in the POST request
+            fetch(`${API_PROTOCOL}://${API_HOSTNAME}/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
@@ -19,49 +36,45 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify({ email, password })
             })
-            .then(response => response.json())
-            .then(data => {
-                if (data.access_token && data.refresh_token) {
-                    const userRole = getRole(data.access_token);
-
-                    if (userRole === 'SUPER_ADMIN' || userRole === 'ADMIN') {
-                        // Login for SUPER_ADMIN or ADMIN
-                        localStorage.setItem('access_token', data.access_token);
-                        localStorage.setItem('refresh_token', data.refresh_token);
-
-                        console.log('User response:', data);
-                        console.log('Login successful');
-                        window.location.href = 'index.html';
+                .then(response => response.json())
+                .then(data => {
+                    if (data.access_token && data.refresh_token) {
+                        const userRole = getRole(data.access_token);
+    
+                        if (userRole === 'SUPER_ADMIN' || userRole === 'ADMIN') {
+                            // Login for SUPER_ADMIN or ADMIN
+                            localStorage.setItem('access_token', data.access_token);
+                            localStorage.setItem('refresh_token', data.refresh_token);
+    
+                            console.log('Login successful');
+                            window.location.href = 'index.html';
+                        } else {
+                            console.log('Login failed: User role not authorized');
+                            staticBackdropModal.show();
+                        }
                     } else {
-                        console.log('Login failed: User role not authorized');
+                        console.log('Invalid login');
                         staticBackdropModal.show();
                     }
-                } else {
-                    console.log('Invalid login');
-                    staticBackdropModal.show();
-                }
-            })
-            .catch(error => {
-                console.error('Error checking login:', error);
-                if (error.response) {
-                    console.error('Response status:', error.response.status);
-                    error.response.text().then(text => {
-                        console.error('Response text:', text);
-                    });
-                }
-            });
-        });
-
-        closeButton.addEventListener('click', () => {
-            staticBackdropModal.hide();
+                })
+                .catch(error => {
+                    console.error('Error checking login:', error);
+                    if (error.response) {
+                        console.error('Response status:', error.response.status);
+                        error.response.text().then(text => {
+                            console.error('Response text:', text);
+                        });
+                    }
+                });
         });
     }
+    
 });
 
 function getRole(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
